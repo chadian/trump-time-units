a827b19848712e86a71b2eb130f2e3441cab2422
Chad Carbert <chadcarbert@me.com>
2017-08-19 20:24:37 +0200

    Add additional tenures

--- src/tenures.js
+++ src/tenures.js
@@ -5,5 +5,25 @@ export default {
   comey: {
     start: inaugration,
     end: "2017-05-10T03:59:59.999Z"
+  },
+  yates: {
+    start: "2017-01-19T05:00:00.000Z",
+    end: "2017-01-30T04:59:59.999Z"
+  },
+  bannon: {
+    start: "2016-08-16T04:00:00.000Z",
+    end: "2017-08-18T03:59:59.999Z"
+  },
+  priebus: {
+    start: inaugration,
+    end: "2017-07-31T03:59:59.999Z"
+  },
+  spicer: {
+    start: "2016-12-21T05:00:00.000Z",
+    end: "2017-07-21T03:59:59.999Z"
+  },
+  flynn: {
+    start: "2017-01-19T05:00:00.000Z",
+    end: "2017-02-13T04:59:59.999Z"
   }
 };
